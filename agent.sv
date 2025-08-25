ea525a8c4274d7f6f876edd659910b441746a567
junhoe99 <147131312+junhoe99@users.noreply.github.com>
2025-08-25 23:20:22 +0900

    Update agent.sv

--- agent.sv
+++ agent.sv
@@ -2,11 +2,9 @@
 class alu_agent extends uvm_agent;
   `uvm_component_utils(alu_agent)
   
-  alu_driver handle_drv; 
-  // driver.sv에서 alu_driver라는 이름의 설계도를 미리 그려서 보유 중.
-  // agent.sv에서 alu_driver라는 설계도로 만든 물건을 담을 빈 그릇(handle)에 drv라는 이름표만 붙여둔 상태 
-  alu_monitor mon; 
-  alu_sequencer seqr;  
+  alu_driver drv;
+  alu_monitor mon;
+  alu_sequencer seqr;
   
     
   //--------------------------------------------------------
@@ -25,9 +23,7 @@ class alu_agent extends uvm_agent;
     super.build_phase(phase);
     `uvm_info("AGENT_CLASS", "Build Phase!", UVM_HIGH)
     
-    handle_drv = alu_driver::type_id::create("drv", this);  
-    // alu_driver라는 설계도를 factory( ::type_id::create() )에 갖고가서 실제 물건(object)를 생성하고, 
-    // 앞서 이름표를 붙여두었던 빈 그릇(handle)인 drv에 담는다
+    drv = alu_driver::type_id::create("drv", this);
     mon = alu_monitor::type_id::create("mon", this);
     seqr = alu_sequencer::type_id::create("seqr", this);
     
@@ -41,7 +37,7 @@ class alu_agent extends uvm_agent;
     super.connect_phase(phase);
     `uvm_info("AGENT_CLASS", "Connect Phase!", UVM_HIGH)
     
-    handle_drv.seq_item_port.connect(seqr.seq_item_export);
+    drv.seq_item_port.connect(seqr.seq_item_export);
     
   endfunction: connect_phase
   
